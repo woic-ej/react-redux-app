ca06e1574ee0229ab8a491162cd834ae04dc1e85
woic-ej <woic0102@naver.com>
2024-03-05 21:55:32 +0900

    Redux thunk 사용

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "./reducers";
+import { fetchPosts } from "./actions/posts";
 
 type Props = {
   value: any;
@@ -9,12 +10,24 @@ type Props = {
   onDecrement: () => void;
 };
 
+interface Post {
+  userId: number;
+  id: number;
+  title: string;
+}
+
 function App({ value, onIncrement, onDecrement }: Props) {
   const dispatch = useDispatch();
   const counter = useSelector((state: RootState) => state.counter);
   const todos: string[] = useSelector((state: RootState) => state.todos);
+  const posts: Post[] = useSelector((state: RootState) => state.posts);
 
   const [todoValue, setTodoValue] = useState("");
+
+  useEffect(() => {
+    dispatch(fetchPosts());
+  }, [dispatch]);
+
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setTodoValue(e.target.value);
   };
@@ -38,6 +51,11 @@ function App({ value, onIncrement, onDecrement }: Props) {
         <input type="text" value={todoValue} onChange={handleChange}></input>
         <input type="submit"></input>
       </form>
+      <ul>
+        {posts.map((post, index) => (
+          <li key={index}>{post.title}</li>
+        ))}
+      </ul>
     </div>
   );
 }
--- src/reducers/index.tsx
+++ src/reducers/index.tsx
@@ -1,10 +1,12 @@
 import { combineReducers } from "redux";
 import counter from "./counter";
 import todos from "./todos";
+import posts from "./posts";
 
 const rootReducer = combineReducers({
   todos,
   counter,
+  posts,
 });
 
 export default rootReducer;
